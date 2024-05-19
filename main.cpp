bfca2f47c610026fc0b9f824089d93abe937dc1c
krezo <kkrezole@gmail.com>
2024-05-19 16:32:16 +0200

    collision detectiow using raylib api

--- main.cpp
+++ main.cpp
@@ -15,40 +15,44 @@
 
 #include "raylib.h"
 
+#define SCREEN_WIDTH (800.0f)
+#define SCREEN_HEIGHT (800.0f)
+
 #define BLOCK_ROWS (5U)
 #define BLOCK_COLS (7U)
+#define BLOCKS_CNT (BLOCK_ROWS * BLOCK_COLS)
 #define BLOCK_WIDTH (50U)
 #define BLOCK_HEIGHT (30U)
 
-#define PADDLE_HEIGHT (30U)
-#define PADDLE_WIDTH (2 * BLOCK_WIDTH)
+#define PADDLE_HEIGHT (30.0f)
+#define PADDLE_WIDTH (100.0f)
 #define PADDLE_Y_BASE_POS (SCREEN_HEIGHT - PADDLE_HEIGHT)
 #define PADDLE_X_BASE_POS ((SCREEN_WIDTH / 2U) - (PADDLE_WIDTH / 2U))
 
-#define BALL_RADIUS (20U)
-#define BALL_X_BASE (SCREEN_WIDTH / 2U)
-#define BALL_Y_BASE (BLOCK_HEIGHT * BLOCK_COLS + 100)
+#define BALL_RADIUS (20.0f)
+#define BALL_X_BASE (SCREEN_WIDTH / 2.0f)
+#define BALL_Y_BASE (BLOCK_HEIGHT * BLOCK_COLS + 100.0f)
 
 static Color colors[BLOCK_ROWS * BLOCK_COLS];
 static Rectangle colorsRecs[BLOCK_ROWS * BLOCK_COLS];
+static Rectangle paddle;
+static Vector2 ball = {.x = BALL_X_BASE, .y = BALL_Y_BASE};
+static Vector2 ballVelocity = {.x = 2.0f, .y = 2.0f};
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
 int main(void) {
   // Initialization
   //--------------------------------------------------------------------------------------
-  const int screenWidth = 800;
-  const int screenHeight = 800;
-
-  InitWindow(screenWidth, screenHeight,
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT,
              "raylib [shapes] example - colors palette");
 
-  for (int i = 0; i < BLOCK_COLS * BLOCK_ROWS; i++) {
+  for (int i = 0; i < BLOCKS_CNT; i++) {
     colors[i] = YELLOW;
   }
 
   // Fills colorsRecs data (for every rectangle)
-  for (int i = 0; i < BLOCK_COLS * BLOCK_ROWS; i++) {
+  for (int i = 0; i < BLOCKS_CNT; i++) {
     colorsRecs[i].width = 100.0f;
     colorsRecs[i].height = 50.0f;
     colorsRecs[i].x = 20.0f + colorsRecs[i].width * (i % BLOCK_COLS) +
@@ -57,10 +61,7 @@ int main(void) {
                       20.0f * (i / BLOCK_COLS);
   }
 
-  int colorState[BLOCK_COLS * BLOCK_ROWS] = {
-      0}; // Color state: 0-DEFAULT, 1-MOUSE_HOVER
-
-  Vector2 mousePoint = {0.0f, 0.0f};
+  int colorState[BLOCKS_CNT] = {0}; // Color state: 0-DEFAULT, 1-MOUSE_HOVER
 
   SetTargetFPS(60); // Set our game to run at 60 frames-per-second
   //--------------------------------------------------------------------------------------
@@ -70,11 +71,28 @@ int main(void) {
   {
     // Update
     //----------------------------------------------------------------------------------
-    mousePoint = GetMousePosition();
+    ball.x += ballVelocity.x;
+    ball.y += ballVelocity.y;
 
-    DrawCircle(mousePoint.x, mousePoint.y, 10.0f, BLUE);
+    if (CheckCollisionPointLine(ball, (Vector2){.x = 0U, .y = 0U},
+                                (Vector2){.x = 0U, .y = SCREEN_HEIGHT}, 1)) {
+      ballVelocity.x *= -1.0f;
+    } else if (CheckCollisionPointLine(
+                   ball, (Vector2){.x = SCREEN_WIDTH, .y = 0U},
+                   (Vector2){.x = SCREEN_WIDTH, .y = SCREEN_HEIGHT}, 1)) {
+      ballVelocity.x *= -1.0f;
+    } else if (CheckCollisionPointLine(ball, (Vector2){.x = 0U, .y = 0U},
+                                       (Vector2){.x = SCREEN_WIDTH, .y = 0U},
+                                       1)) {
+      ballVelocity.y *= -1.0f;
+    } else if (CheckCollisionPointLine(
+                   ball, (Vector2){.x = 0U, .y = SCREEN_HEIGHT},
+                   (Vector2){.x = SCREEN_WIDTH, .y = SCREEN_HEIGHT}, 1)) {
+      ballVelocity.y *= -1.0f;
+    }
+
     for (int i = 0; i < BLOCK_COLS * BLOCK_ROWS; i++) {
-      if (CheckCollisionCircleRec(mousePoint, 10.0f, colorsRecs[i])) {
+      if (CheckCollisionCircleRec(ball, 10.0f, colorsRecs[i])) {
         colorState[i] = 1;
         colors[i] = BLUE;
       } else {
@@ -89,10 +107,9 @@ int main(void) {
 
     ClearBackground(RAYWHITE);
 
-    DrawText("raylib colors palette", 28, 42, 20, BLACK);
-    DrawText("press SPACE to see all colors", GetScreenWidth() - 180,
-             GetScreenHeight() - 40, 10, GRAY);
+    DrawText("Krezo breakout clone ", 28, 42, 20, BLACK);
 
+    DrawCircle(ball.x, ball.y, 10.0f, BLUE);
     for (int i = 0; i < BLOCK_COLS * BLOCK_ROWS; i++) // Draw all rectangles
     {
       // DrawRectangleRec(colorsRecs[i],
